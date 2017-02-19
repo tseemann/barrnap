c44e0b0a5347a678124ea275196be47f82f83d06
Torsten Seemann <torsten.seemann@gmail.com>
2017-02-19 15:53:46 +1100

    Update database builder script to support new SILVA download

--- build/build_HMMs.sh
+++ build/build_HMMs.sh
@@ -1,57 +1,58 @@
 #!/bin/bash
 
 CPUS=$(grep -c bogomips /proc/cpuinfo)
+CURL="curl"
+GUNZIP="gzip -c"
 
 RFAM="Rfam.seed"
+RFAMURL="ftp://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/${RFAM}.gz"
 if [ ! -r "$RFAM" ]; then
   echo "Downloading: $RFAM"
-  wget --quiet ftp://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT/Rfam.seed.gz 
-  gunzip $RFAM.gz
+  $CURL "$RFAMURL" | $GUNZIP -c > "$RFAM"
 else
   echo "Using existing file: $RFAM"
 fi
 
-
 # 23S only as 16S is in RFAM
-SILVA="LSURef_115_tax_silva_full_align_trunc.fasta"
+SILVA="SILVA_128_LSURef_tax_silva_full_align_trunc.fasta"
+SILVAURL="http://www.arb-silva.de/fileadmin/silva_databases/current/Exports/${SILVA}.gz"
 if [ ! -r "$SILVA" ]; then
   echo "Downloading: $SILVA"
-  wget --quiet http://www.arb-silva.de/fileadmin/silva_databases/current/Exports/LSURef_115_tax_silva_full_align_trunc.fasta.tgz
-  tar zxf $SILVA.tgz
-  rm -f $SILVA.tgz
+  $CURL "$SILVAURL" | $GUNZIP -c > "$SILVA"
 else
   echo "Using existing file: $SILVA"
 fi
 
 # this will write three files: LSU.Kingdom.aln
-./fix-SILVA.pl --seed --type LSU $SILVA
+echo "Fixing and splitting SILVA data"
+./fix-SILVA.pl --seed --type LSU "$SILVA"
 
 # Prepare RFAM for fetches
 echo "Indexing $RFAM"
-rm -f $RFAM.ssi  
-esl-afetch --index $RFAM
+rm -f "$RFAM.ssi"
+esl-afetch --index "$RFAM"
 
 echo "Fetching models..."
 
 # Bact
 echo "Bac"
-esl-afetch $RFAM RF00001 > 5S.bac.aln
+esl-afetch "$RFAM" RF00001 > 5S.bac.aln
 esl-reformat -r stockholm LSU.Bacteria.aln > 23S.bac.aln
-esl-afetch $RFAM RF00177 > 16S.bac.aln
+esl-afetch "$RFAM" RF00177 > 16S.bac.aln
 
 # Arch
 echo "Arc"
-esl-afetch $RFAM RF00001 > 5S.arc.aln
-esl-afetch $RFAM RF00002 > 5_8S.arc.aln
+esl-afetch "$RFAM" RF00001 > 5S.arc.aln
+esl-afetch "$RFAM" RF00002 > 5_8S.arc.aln
 esl-reformat -r stockholm LSU.Archaea.aln > 23S.arc.aln
-esl-afetch $RFAM RF01959 > 16S.arc.aln
+esl-afetch "$RFAM" RF01959 > 16S.arc.aln
 
 # Euk
 echo "Euk"
-esl-afetch $RFAM RF00001 > 5S.euk.aln
-esl-afetch $RFAM RF00002 > 5_8S.euk.aln
+esl-afetch "$RFAM" RF00001 > 5S.euk.aln
+esl-afetch "$RFAM" RF00002 > 5_8S.euk.aln
 esl-reformat -r stockholm LSU.Eukaryota.aln > 28S.euk.aln
-esl-afetch $RFAM RF01960 > 18S.euk.aln
+esl-afetch "$RFAM" RF01960 > 18S.euk.aln
 
 # Mito
 FILE="12S.mito.aln"
@@ -67,7 +68,7 @@ fi
 
 
 for K in arc bac euk mito ; do
-  for T in 5S 5_8S 16S 23S 28S ; do 
+  for T in 5S 5_8S 12S 16S 23S 28S ; do 
     ID="$T.$K"
     if [ -r "$ID.aln" ]; then
       echo "*** $ID ***"
@@ -90,7 +91,7 @@ exit
 for ID in $(cat MODELS) ; do
 
   echo "Extracting: $ID.aln"
-  esl-afetch $RFAM $ID > $ID.aln
+  esl-afetch "$RFAM" $ID > $ID.aln
 
   echo "Building: $ID.hmm"
   rm -f $ID.hmm.h?? $ID.hmm
